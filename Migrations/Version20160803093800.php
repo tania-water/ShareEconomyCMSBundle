e8b40b191768ea2beab167e0be56dbcf95c281c3
Mahmoud Mostafa <mahmoud.mostafa@ibtikar.net.sa>
2016-08-03 09:58:04 +0200

    Enhancement: Update the migration file to include the static pages

--- Migrations/Version20160803093800.php
+++ Migrations/Version20160803093800.php
@@ -10,6 +10,7 @@ use Doctrine\DBAL\Schema\Schema;
  */
 class Version20160803093800 extends AbstractMigration
 {
+
     /**
      * @param Schema $schema
      */
@@ -20,6 +21,13 @@ class Version20160803093800 extends AbstractMigration
 
         $this->addSql('ALTER TABLE page ADD titleAr VARCHAR(255) NOT NULL, ADD contentAr LONGTEXT DEFAULT NULL');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_140AB620CE671CFF ON page (titleAr)');
+
+        $this->addSql("
+            INSERT INTO `page` (`title`, `slug`, `content`, `created`, `updated`, `titleAr`, `contentAr`) VALUES
+            ('About', 'about', NULL, '2016-08-03 09:40:03', '2016-08-03 09:40:03', 'عن الموقع', NULL),
+            ('Privacy Policy', 'privacy-policy', NULL, '2016-08-03 09:40:03', '2016-08-03 09:40:03', 'سياسة الخصوصية', NULL),
+            ('Terms and conditions', 'terms-and-conditions', NULL, '2016-08-03 09:40:03', '2016-08-03 09:40:03', 'الشروط و الأحكام', NULL);
+        ");
     }
 
     /**
@@ -32,5 +40,6 @@ class Version20160803093800 extends AbstractMigration
 
         $this->addSql('DROP INDEX UNIQ_140AB620CE671CFF ON page');
         $this->addSql('ALTER TABLE page DROP titleAr, DROP contentAr');
+        $this->addSql("DELETE FROM page WHERE slug IN('about', 'privacy-policy', 'terms-and-conditions');");
     }
 }
