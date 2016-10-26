689b118fe61c98f1ede58ba7e268d618a0235e45
Mahmoud Mostafa <mahmoud.mostafa@ibtikar.net.sa>
2016-10-26 16:05:11 +0200

    Enhancement: Fix the old migrations add default value

--- Migrations/Version20161020123011.php
+++ Migrations/Version20161020123011.php
@@ -18,9 +18,9 @@ class Version20161020123011 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE cms_contact_type ADD created_at DATETIME NOT NULL, ADD updated_at DATETIME NOT NULL, ADD version INT DEFAULT 1 NOT NULL');
-        $this->addSql('ALTER TABLE cms_contact ADD updated_at DATETIME NOT NULL, ADD version INT DEFAULT 1 NOT NULL, CHANGE created_at created_at DATETIME NOT NULL');
-        $this->addSql('ALTER TABLE page ADD created_at DATETIME NOT NULL, ADD updated_at DATETIME NOT NULL, ADD version INT DEFAULT 1 NOT NULL, DROP created, DROP updated');
+        $this->addSql('ALTER TABLE cms_contact_type ADD created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, ADD updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, ADD version INT DEFAULT 1 NOT NULL');
+        $this->addSql('ALTER TABLE cms_contact ADD updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, ADD version INT DEFAULT 1 NOT NULL, CHANGE created_at created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP');
+        $this->addSql('ALTER TABLE page ADD created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, ADD updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, ADD version INT DEFAULT 1 NOT NULL, DROP created, DROP updated');
     }
 
     /**
